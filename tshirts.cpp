c4c6bb9ff10ef8a53079fad3e93b68c6875ad91f
Dhruva1506 <vidushikumari15@gmail.com>
2025-08-04 11:19:18 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -16,20 +16,20 @@ char size(int cms) {
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
 
-    // Original tests
+    
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
 
-    // New tests to expose boundary bugs:
-    // These should FAIL with the current buggy implementation
-    assert(size(38) != '\0'); // 38 should be classified, currently returns '\0'
-    assert(size(42) != '\0'); // 42 should be classified, currently returns '\0'
+    
+    assert(size(38) != '\0'); 
+    assert(size(42) != '\0'); 
 
-    // More specific expected classifications:
-    assert(size(38) == 'M'); // expected size for 38 cm
-    assert(size(42) == 'M'); // expected size for 42 cm
+   
+    assert(size(38) == 'M'); 
+    assert(size(42) == 'M');
 
     std::cout << "All tests passed (this should not print if bugs exist)\n";
 }
+
 
